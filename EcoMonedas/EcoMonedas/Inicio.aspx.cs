b53f34e8fbfa1da164afe1dc53b844ae7deaa1d8
Erick Rodriguez Vega <erickrvrv1717@gmail.com>
2019-03-01 00:23:47 -0600

    Avance 1/3/2019 Añadi Bootstrap y comence el Index junto a la primera pagina Maestra

---
+++ EcoMonedas/EcoMonedas/Inicio.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EcoMonedas
+{
+    public partial class Inicio : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void navbarColor01_MenuItemDataBound(object sender, MenuEventArgs e)
+        {
+            SiteMapNode smn = (SiteMapNode)e.Item.DataItem;
+            e.Item.ImageUrl = smn["image"];
+        }
+    }
+}
