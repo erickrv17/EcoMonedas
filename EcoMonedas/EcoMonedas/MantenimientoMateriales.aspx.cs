f38531e85902dbea3bb03327b432517327da2150
Erick Rodriguez Vega <erickrvrv1717@gmail.com>
2019-03-15 12:03:19 -0600

    Comienzo de Administrador de Centro de Acopio Mas que añadi una Template

--- EcoMonedas/EcoMonedas/MantenimientoMateriales.aspx.cs
+++ EcoMonedas/EcoMonedas/MantenimientoMateriales.aspx.cs
@@ -32,59 +32,59 @@ namespace EcoMonedas
 
         protected void btnGuardar_Click(object sender, EventArgs e)
         {
-            Boolean archivoOK = false;
-            String path = Server.MapPath("~/Imagenes/");
-            if (archivoImagen.HasFile)
-            {
-                String fileExtension = System.IO.Path.GetExtension(archivoImagen.FileName).ToLower();
-                String[] allowedExtensions = { ".gif", ".png", ".jpeg", ".jpg" };
-                for (int i = 0; i < allowedExtensions.Length; i++)
-                {
-                    if (fileExtension == allowedExtensions[i])
-                    {
-                        archivoOK = true;
-                    }
-                }
-            }
+            //Boolean archivoOK = false;
+            //String path = Server.MapPath("~/Imagenes/");
+            //if (archivoImagen.HasFile)
+            //{
+            //    String fileExtension = System.IO.Path.GetExtension(archivoImagen.FileName).ToLower();
+            //    String[] allowedExtensions = { ".gif", ".png", ".jpeg", ".jpg" };
+            //    for (int i = 0; i < allowedExtensions.Length; i++)
+            //    {
+            //        if (fileExtension == allowedExtensions[i])
+            //        {
+            //            archivoOK = true;
+            //        }
+            //    }
+            //}
 
-            if (archivoOK)
-            {
-                try
-                {
-                    archivoImagen.PostedFile.SaveAs(path + archivoImagen.FileName);
-                }
-                catch (Exception ex)
-                {
-                    lblMensaje.Visible = true;
-                    lblMensaje.Text = ex.Message;
-                }
+            //if (archivoOK)
+            //{
+            //    try
+            //    {
+            //        archivoImagen.PostedFile.SaveAs(path + archivoImagen.FileName);
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        lblMensaje.Visible = true;
+            //        lblMensaje.Text = ex.Message;
+            //    }
                 
-                bool confirmar = MaterialLN.GuardarMaterial(txtNombre.Text,archivoImagen.FileName,Convert.ToDecimal(txtPrecio.Text),ddlColor.SelectedValue, chkEstado.Checked, HiddenField1.Value);
-                if (confirmar)
-                {
+            //    bool confirmar = MaterialLN.GuardarMaterial(txtNombre.Text,archivoImagen.FileName,Convert.ToDecimal(txtPrecio.Text),ddlColor.SelectedValue, chkEstado.Checked, HiddenField1.Value);
+            //    if (confirmar)
+            //    {
 
-                    // Recargar la pagina
-                    string accion = (HiddenField1.Value == "" || HiddenField1.Value == "0") ? "nuevo" : "actu";
-                    Response.Redirect("MantenimientoMateriales.aspx?accion=" + accion);
-                }
-                else
-                {
-                    lblMensaje.Visible = true;
-                    lblMensaje.Text = "No se puede agregar un nuevo Material a la BD";
-                }
-            }
-            else
-            {
+            //        // Recargar la pagina
+            //        string accion = (HiddenField1.Value == "" || HiddenField1.Value == "0") ? "nuevo" : "actu";
+            //        Response.Redirect("MantenimientoMateriales.aspx?accion=" + accion);
+            //    }
+            //    else
+            //    {
+            //        lblMensaje.Visible = true;
+            //        lblMensaje.Text = "No se puede agregar un nuevo Material a la BD";
+            //    }
+            //}
+            //else
+            //{
 
-                lblMensaje.Visible = true;
-                lblMensaje.Text = "No se puede aceptar el tipo de archivo.";
-            }
+            //    lblMensaje.Visible = true;
+            //    lblMensaje.Text = "No se puede aceptar el tipo de archivo.";
+            //}
         }
 
         protected void grvListado_SelectedIndexChanged(object sender, EventArgs e)
         {
             int id = Convert.ToInt32(grvListado.DataKeys[grvListado.SelectedIndex].Values[0]);
-            Material mat = MaterialLN.obte(id);
+            Material mat = MaterialLN.obtenerMaterial(id);
             ddlColor.SelectedValue = mat.IdColor.ToString();
             txtNombre.Text = mat.Nombre;
             Image1.ImageUrl = "~/Imagenes/" + mat.Imagen;
