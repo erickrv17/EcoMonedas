d325cfddd579a9a20c511a843eb32f82bfb8c640
cet21 <cet21@DESKTOP-3HPN40L>
2019-03-23 02:01:15 -0600

    login

--- EcoMonedas/EcoMonedas/MasterCliente.Master.cs
+++ EcoMonedas/EcoMonedas/MasterCliente.Master.cs
@@ -13,5 +13,51 @@ namespace EcoMonedas
         {
 
         }
+        protected void btnIngresar_Click(object sender, EventArgs e)
+        {
+        //    UsuarioLogica usuarioLogica = new UsuarioLogica();
+        //    VistanteLogica vistanteLogica = new VistanteLogica();
+        //    Usuario user = null;
+        //    user = usuarioLogica.Login((txtIdLogin.Value), txtContrasenna.Value);
+        //    if (user == null)
+        //    {
+        //        txtErrorUsuario.Visible = true;
+        //        ScriptManager.RegisterStartupScript(this, this.GetType(), "Pop", "openModalLogin();", true);
+
+        //        // return;
+        //    }
+        //    else
+        //    {
+        //        Visitante visitante = new Visitante();
+        //        visitante.Usuario = user;
+        //        visitante.FechaVisita = DateTime.Now;
+        //        vistanteLogica.Guardar(visitante);
+        //        Session["NumeroVisitantes"] = vistanteLogica.NumeroVisitas();
+
+        //        Session["Usuario"] = user;
+        //        Session["Nombre"] = user.Nombre;
+        //        if (user.TipoUsuario.Equals(TipoUsuario.Cliente))
+        //        {
+        //            txtNombre.Visible = true;
+        //            btnMantenimientos.Visible = false;
+        //            btnCerrarS.Visible = true;
+        //            btnRegistrarse.Visible = false;
+        //            btnLogin.Visible = false;
+
+        //        }
+        //        else
+        //        {
+        //            txtNombre.Visible = true;
+        //            btnCerrarS.Visible = true;
+        //            btnMantenimientos.Visible = true;
+        //            btnRegistrarse.Visible = false;
+        //            btnLogin.Visible = false;
+        //            btnAgendaVisitas.Visible = true;
+        //        }
+
+
+
+        //    }
+        }
     }
 }
--- EcoMonedas/EcoMonedas/MasterCliente.Master.designer.cs
+++ EcoMonedas/EcoMonedas/MasterCliente.Master.designer.cs
@@ -29,5 +29,14 @@ namespace EcoMonedas {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// Control btnIngresar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnIngresar;
     }
 }
